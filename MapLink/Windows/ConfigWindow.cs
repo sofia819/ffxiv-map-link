347dd54da9e5fb1bac3993dbc34717fdde408395
Sofia Lee <sofia819@gmail.com>
2024-08-12 20:15:07 -0400

    Validate player name and use table to show players

--- MapLink/Windows/ConfigWindow.cs
+++ MapLink/Windows/ConfigWindow.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Numerics;
+using Dalamud.Interface;
+using Dalamud.Interface.Components;
 using Dalamud.Interface.Windowing;
 using ImGuiNET;
 using ImGui = ImGuiNET.ImGui;
@@ -17,8 +19,8 @@ public class ConfigWindow : Window, IDisposable
 
         SizeConstraints = new WindowSizeConstraints
         {
-            MinimumSize = new Vector2(150, 200),
-            MaximumSize = new Vector2(300, float.MaxValue)
+            MinimumSize = new Vector2(220, 200),
+            MaximumSize = new Vector2(220, 350)
         };
 
         SizeCondition = ImGuiCond.Always;
@@ -56,7 +58,10 @@ public class ConfigWindow : Window, IDisposable
         ImGui.Spacing();
 
         // Player input box
+
+
         var buffer = "";
+        ImGui.PushItemWidth(200);
         if (
             ImGui.InputTextWithHint(
                 "",
@@ -68,36 +73,56 @@ public class ConfigWindow : Window, IDisposable
         )
         {
             var playerName = buffer;
-            configuration.Players[playerName] = true;
-            configuration.Save();
+            Plugin.ChatGui.Print(
+                configuration.SavePlayerName(playerName)
+                    ? $"{playerName} added successfully"
+                    : $"Failed to add {playerName}",
+                Plugin.PluginName
+            );
         }
+        ImGui.PopItemWidth();
 
         ImGui.Spacing();
 
+        if (configuration.Players.Count == 0)
+            return;
+
         // Player list
-        foreach (var player in configuration.Players)
+        if (ImGui.BeginTable("Players", 3, ImGuiTableFlags.Borders))
         {
-            var isPlayerEnabled = player.Value;
+            ImGui.TableSetupColumn("X", ImGuiTableColumnFlags.WidthFixed);
+            ImGui.TableSetupColumn("O", ImGuiTableColumnFlags.WidthFixed);
+            ImGui.TableSetupColumn("Player");
 
-            if (ImGui.Checkbox(player.Key, ref isPlayerEnabled))
+            foreach (var player in configuration.Players)
             {
-                configuration.Players[player.Key] = isPlayerEnabled;
-                configuration.Save();
-            }
+                ImGui.PushID(player.Key);
 
-            ImGui.SameLine();
-            ImGui.PushStyleColor(ImGuiCol.Button, new Vector4(255, 0, 0, 255));
-            ImGui.PushID(player.Key);
-            if (ImGui.SmallButton("X"))
-            {
-                configuration.Players.Remove(player.Key);
-                configuration.Save();
+                // Remove player
+                ImGui.TableNextColumn();
+                if (ImGuiComponents.IconButton(FontAwesomeIcon.Trash))
+                {
+                    configuration.Players.Remove(player.Key);
+                    configuration.Save();
+                    ImGui.PopID();
+                }
+
+                // Enable player
+                ImGui.TableNextColumn();
+                var isPlayerEnabled = player.Value;
+                if (ImGui.Checkbox("", ref isPlayerEnabled))
+                {
+                    configuration.Players[player.Key] = isPlayerEnabled;
+                    configuration.Save();
+                }
+
+                // Player name
+                ImGui.TableNextColumn();
+                ImGui.Text(player.Key);
+
                 ImGui.PopID();
             }
-
-            ImGui.PopStyleColor();
-
-            ImGui.Spacing();
         }
+        ImGui.EndTable();
     }
 }
